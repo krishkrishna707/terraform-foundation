b7df52815d4d54e1c08719f007bd9fb1ef865fa1
Krishna Raj <krishna.raj@tisteps.co>
2022-02-14 08:00:13 +0530

    tf files

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "REGION" {
+    default = "us-east-1"
+}
+variable "KEY_NAME" {
+    default = "test"
+}
+variable "PUBLIC_KEY" {
+    default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6t"
+}
